6b84671c8b792c0aaecaeb823c27e1c8c3911ee9
luca di marco <luca.dimarco96@gmail.com>
2020-01-08 09:55:04 +0100

    remove plugin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,8 +6,7 @@ import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 })
 export class HomePage {
 
-  constructor(private screenOrientation: ScreenOrientation) {
-    console.log(this.screenOrientation.type);
+  constructor() {
   }
 
 }
