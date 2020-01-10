f2901980a57b185573228d5585a9dc38a7072be9
nicholas.angelucci@gmail.com <kNbPDyCY7GR7GHN>
2020-01-10 15:40:25 +0100

    .

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -17,6 +17,8 @@ import { EMPTY, of } from 'rxjs';
 })
 export class HomePage {
 
+  mana: Array<number> = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   constructor(
     private activatedRoute: ActivatedRoute,
     public loadingController: LoadingController,
